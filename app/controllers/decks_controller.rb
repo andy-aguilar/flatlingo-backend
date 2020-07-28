d67c62fe51d7fdfba11b7b79d32ae8767cc05f3b
andy-aguilar <aaguil3@gmail.com>
2020-07-28 15:45:19 -0400

    created show controllers for users and decks

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,2 +1,7 @@
 class DecksController < ApplicationController
+
+    def show
+        deck = Deck.find(params[:id])
+        render json: deck, include: [:cards]
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Deck.destroy_all
+Card.destroy_all
+User.destroy_all
+
+flatiron = User.create(username: "Flatiron")
+
+beer = Deck.create(name: "Beer", user_id: flatiron.id)
+
+10.times do 
+    Card.create(front_word: Faker::Beer.brand, back_definition: Faker::Beer.name, back_notes: Faker::Beer.style, understanding: false, understanding_num: 0, deck_id: beer.id)
+end
+
