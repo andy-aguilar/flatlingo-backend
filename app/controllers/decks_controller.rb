001f4e29f42513105a90694b80d8e62f6217a978
zato91 <zatokey@gmail.com>
2020-07-28 23:08:06 -0400

    Create method for decks controller

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,25 @@
 class DecksController < ApplicationController
+    def index
+        decks = Deck.all
+        render json: decks, include: [:cards]
+    end
 
     def show
         deck = Deck.find(params[:id])
         render json: deck, include: [:cards]
+    end
+
+
+    def create
+        user = params['user_id']
+        deck = Deck.create(name: params["name"], user_id: user);#  
+        render json: deck, include: [:cards]
+
+    end 
+
+    private 
+
+    def deckParams
+        params.require(:deck).permit(:name, :user_id)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, include: [:decks]
+    end
+
     def show
         user = User.find(params[:id])
         render json: user, include: [:decks]
     end
+
+
 end
