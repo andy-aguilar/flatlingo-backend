96baec28cfd0cee3ead2c65d80507dad0a8a5d4f
andy-aguilar <aaguil3@gmail.com>
2020-07-29 07:13:36 -0400

    created functionality for users and decks index and show

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,21 @@
 class DecksController < ApplicationController
+    def index
+        decks = Deck.all
+        render json: decks, include: :cards
+    end
 
     def show
         deck = Deck.find(params[:id])
         render json: deck, include: [:cards]
+    end
+
+    def create
+        deck = Deck.create(deck_params)
+        render json: deck
+    end
+
+    private
+    def deck_params
+        params.require(:deck).permit(:name, :user_id)
     end
 end
