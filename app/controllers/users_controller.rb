8092cfd9682b6ed586b3be44f756f2691c9d9649
andy-aguilar <aaguil3@gmail.com>
2020-07-30 17:30:33 -0400

    added signup feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,19 @@ class UsersController < ApplicationController
         render json: user, include: [:decks]
     end
 
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else 
+            render json: user.errors.full_messages
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :decks
     has_many :cards, through: :decks
+    validates :username, uniqueness: true
 end
