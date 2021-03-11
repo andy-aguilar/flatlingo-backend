c58b0c5ca01464e31c96173553fc8b87fa187eec
andy-aguilar <aaguil3@gmail.com>
2021-03-11 14:58:53 -0600

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_decks
-  resources :cards
-  resources :decks
-  resources :users
+  resources :user_decks, only: :create
+  resources :cards, only: [:create, :edit, :update, :destroy]
+  resources :decks, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :login, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/users/login/:username", to: "users#login"
