6b346ec5ea5c7d399909b63fee98bd7ba7eaf725
andy-aguilar <aaguil3@gmail.com>
2021-03-11 14:12:32 -0600

    refactor find_card in CardsController to before action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,16 +1,12 @@
 class CardsController < ApplicationController
-    def index
-        cards = Card.all
-        render json: cards
-    end
-
+    before_action :find_card, only: [:edit, :update, :destroy]
     def create
         card = Card.create(card_params)
         render json: card
     end 
 
     def edit 
-        card = Card.find(params[:id])
+        render json: card
     end 
 
     def update
@@ -31,4 +27,8 @@ class CardsController < ApplicationController
     def card_params 
         params.require(:card).permit(:front_word, :back_definition, :back_notes, :understanding, :understanding_num, :deck_id)
     end 
+
+    def find_card
+        card = Card.find(params[:id])
+    end
 end
